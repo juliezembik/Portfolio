eceb42cfdbdd6dc9e70445f551a0e3e231445fd2
Julie Zembik <kurokurosan@gmail.com>
2019-02-01 16:39:00 -0600

    add new components for project and base code, added missing modules

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Project from './../Project/Project';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <h1>Portfolio</h1>
+        <Project />
       </div>
     );
   }
---
+++ src/components/Project/ProjectList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ProjectList extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default ProjectList;
